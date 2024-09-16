3516e011fbdc94c61e7ffb87c48cdf0f4f213057
Wasim2708 <101546593+Wasim2708@users.noreply.github.com>
2024-09-16 12:24:35 +0530

    Update Package to beta 2

--- Package.swift
+++ Package.swift
@@ -9,17 +9,18 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ZohoDeskPortalCorePackage",
-            targets: ["ZohoDeskPortalCorePackage"]),
+            targets: ["ZohoDeskPortalCorePackage", "ZohoDeskPortalCore"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Wasim2708/ZohoDeskPortalUIKitPackage.git", exact: "1.0.0"),
-        .package(url: "https://github.com/Wasim2708/ZohoDeskPortalKBPackage.git", exact: "1.0.1"),
-        .package(url: "https://github.com/Wasim2708/ZohoDeskPortalCommunityPackage.git", exact: "1.0.1"),
-        .package(url: "https://github.com/Wasim2708/ZohoDeskPortalTicketPackage.git", exact: "1.0.1"),
+        .package(url: "https://github.com/Wasim2708/ZohoDeskPortalUIKitPackage.git", exact: "1.0.2"),
+        .package(url: "https://github.com/Wasim2708/ZohoDeskPortalKBPackage.git", exact: "1.0.2"),
+        .package(url: "https://github.com/Wasim2708/ZohoDeskPortalCommunityPackage.git", exact: "1.0.2"),
+        .package(url: "https://github.com/Wasim2708/ZohoDeskPortalTicketPackage.git", exact: "1.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(name: "ZohoDeskPortalCore", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalCore/4.0.0-beta.2/ZohoDeskPortalCore.zip", checksum: "866f5bbb9c369d166b364880ec0a32abdd10dd8b248c0eeabcf38fbb792949e9"),
         .target(
             name: "ZohoDeskPortalCorePackage",
             dependencies: [
